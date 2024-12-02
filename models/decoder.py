5e4efa046cb1435e0a5e39e08a41fefd30d13a47
Seyed Ashkan Zeinolabedini <ashkan.zeinolabedini@mail.utoronto.ca>
2024-12-02 10:26:58 -0500

    fixed dimension issue

--- models/decoder.py
+++ models/decoder.py
@@ -8,7 +8,7 @@ class Decoder(nn.Module):
         self.embed = nn.Embedding(vocab_size, embed_size)
         # 修改输入维度为 embed_size + 768 (ViT特征维度)
         self.lstm = nn.LSTM(
-            input_size=embed_size + 768,
+            input_size=embed_size * 2,
             hidden_size=hidden_size,
             num_layers=num_layers,
             batch_first=True,
@@ -21,7 +21,7 @@ class Decoder(nn.Module):
         # 将图像特征扩展并与每个时间步的词嵌入连接
         features = features.unsqueeze(1).repeat(1, embeddings.size(1), 1)
         lstm_input = torch.cat((features, embeddings), dim=2)
-        
+        #print(f"features: {features.size()} embeddings: {embeddings.size()} lstm_input: {lstm_input.size()}")
         lstm_out, _ = self.lstm(lstm_input)
         outputs = self.linear(lstm_out)
         
