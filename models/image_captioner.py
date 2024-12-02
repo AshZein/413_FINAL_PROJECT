fixed batch size calculation

models/image_captioner.py
@@ -16,25 +16,32 @@ class ImageCaptioner(nn.Module):
         return outputs
 
     def generate_caption(self, image, vocab, max_length=20):
+        batch_size = image.size()[0]
         features = self.encoder(image)
         
         # 生成描述
         states = None
-        inputs = torch.tensor([[vocab.word2idx['<START>']]], device=image.device)
-        caption = []
+        inputs = torch.tensor([[vocab.word2idx['<START>']]] * batch_size, device=image.device)
+        caption = [[] for l in range(batch_size)]
         
         for _ in range(max_length):
             embeddings = self.decoder.embed(inputs)
             features_repeat = features.unsqueeze(1)
+            #print(f"features_repeat {features_repeat.size()} embeddings {embeddings.size()}")
             lstm_input = torch.cat((features_repeat, embeddings), dim=2)
             lstm_out, states = self.decoder.lstm(lstm_input, states)
             outputs = self.decoder.linear(lstm_out.squeeze(1))
             predicted = outputs.argmax(1)
+            #print(f"predicted size {predicted.size()} captions size: {len(caption)} batch size: {batch_size}")
             
-            if predicted.item() == vocab.word2idx['<END>']:
-                break
+            for i in range(predicted.size()[0]):
+                if predicted[i].item() != vocab.word2idx['<END>']:
+                    caption[i].append(predicted[i].item())
+                    inputs[i] = predicted[i].unsqueeze(0).unsqueeze(0)
+            # if predicted.item() == vocab.word2idx['<END>']:
+            #     break
                 
-            caption.append(predicted.item())
-            inputs = predicted.unsqueeze(0).unsqueeze(0)
+            #caption.append(predicted.item())
+            # inputs = predicted.unsqueeze(0).unsqueeze(0)
             
         return caption 

train.py
@@ -59,25 +59,41 @@ def validate(model, dataloader, vocab, device):
     with torch.no_grad():
         for images, captions in tqdm(dataloader, desc='Validating'):
             images = images.to(device)
-            
+            #print(f"number of captions: {len(captions)}")
             # 生成描述
             predicted_ids = model.generate_caption(images, vocab)
             
             # 转换为文本
-            predicted_caption = [vocab.idx2word[idx] for idx in predicted_ids 
-                               if idx not in [vocab.word2idx['<START>'], 
-                                            vocab.word2idx['<END>'], 
-                                            vocab.word2idx['<PAD>']]]
+            predicted_caption = [[] for i in range(len(predicted_ids))]
+            for i in range(len(predicted_ids)):
+                for idx in predicted_ids[i]:
+                    if idx not in [vocab.word2idx['<START>'], vocab.word2idx['<END>'], vocab.word2idx['<PAD>']]:
+                        predicted_caption[i].append(vocab.idx2word[idx])
+                        
+            # predicted_caption = [vocab.idx2word[idx] for idx in predicted_ids 
+            #                    if idx not in [vocab.word2idx['<START>'], 
+            #                                 vocab.word2idx['<END>'], 
+            #                                 vocab.word2idx['<PAD>']]]
             
-            reference_caption = [[vocab.idx2word[idx.item()] for idx in caption[1:-1]]  
-                               for caption in captions]
+            # reference_caption = [[] for i in range(len(predicted_ids))]
+            # for caption in captions:
+            #     for idx in caption[1:-1]:
+                
+            # reference_caption = [[] for l in range(len(captions))]    
+            # for i in range(len(captions)):
+            #     for idx in captions[i][1:-1]:
+            #         reference_caption[i].append(vocab.idx2word[idx.item()])
+            reference_caption = [[vocab.idx2word[idx.item()] for idx in caption[1:-1]] for caption in captions]
+            
             
             all_predictions.extend([predicted_caption])
             all_references.extend([reference_caption])
     
     # 计算BLEU分数
-    bleu4 = calculate_bleu(all_references, all_predictions)
-    return bleu4
+    bleu4 = 0.0
+    for i in range(len(all_predictions)):
+        bleu4 += calculate_bleu(all_references, all_predictions[i])
+    return bleu4 / len(all_predictions)
 
 def main(args):
     # 设置设备