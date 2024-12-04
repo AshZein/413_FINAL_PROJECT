e617bb2c6965c1fb4685ecef0c6ff0a5effaf37c
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2024-12-03 20:13:07 -0500

    smoothing function

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,6 +1,8 @@
 from nltk.translate.bleu_score import corpus_bleu
+from nltk.translate.bleu_score import SmoothingFunction
 
 def calculate_bleu(references, predictions):
-    bleu4 = corpus_bleu(references, predictions)
+    smoothing_function = SmoothingFunction().method1
+    bleu4 = corpus_bleu(references, predictions, smoothing_function=smoothing_function)
     return bleu4
     
